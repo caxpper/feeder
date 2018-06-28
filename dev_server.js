4e263055875ad9283f1d6fb362b1d8c46a7f0768
Alejandro Gaspar <caxpper@hotmail.com>
2018-06-28 15:55:39 -0700

    fixed header for node

--- dev_server.js
+++ dev_server.js
@@ -12,7 +12,7 @@ endPoint.use(express.static(__dirname + '/public'));
 endPoint.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'null');
+    res.setHeader('Access-Control-Allow-Origin', 'http://feed.alejandro-gaspar.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
